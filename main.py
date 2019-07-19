6eb1e5f527adaa11f0bc802ab61aa0a1e4568869
Janse Van Rensburg <s213295245@Mandela.ac.za>
2019-07-19 13:53:35 +0200

    Prepared for easier identify and download of Single video vs Playlist

--- main.py
+++ main.py
@@ -1,52 +1,90 @@
 from pytube import YouTube 	#Download Youtube Videos
 from pytube import Playlist #Download Youtube Playlists
+import pytube
 import os
 
 #Youtube Link
-downloadLink = "https://www.youtube.com/watch?v=E1ZVSFfCk9g"
+downloadLink = "https://www.youtube.com/watch?v=M_EoNNlRgn4"
+#downloadLink = "https://www.youtube.com/watch?v=Edpy1szoG80&list=PL153hDY-y1E00uQtCVCVC8xJ25TYX8yPU"
 
 #Download Directory
 #path = "C:\\Users\\s213295245\\Documents\\YoutubeDownloads"
 
-#YouTube(downloadLink).streams.first().download(path)	#One Liner Working
 
-#SINGLE IMAGE DOWNLOAD
-# Create Single Videos Directory if it doesn't exist
-if not os.path.exists("SingleVideos"):
-    os.mkdir("SingleVideos")
-    print("Directory " , "SingleVideos" ,  " Created ")
-else:    
-    print("Directory " , "SingleVideos" ,  " already exists")
+def singleVideoDownload():
+	# Create Single Videos Directory if it doesn't exist
+	if not os.path.exists("SingleVideos"):
+		os.mkdir("SingleVideos")
+		print("Directory SingleVideos Created")
+	else:
+		print("Directory SingleVideos already exists")
+    #One Liner Working
+	#YouTube(downloadLink).streams.first().download(".\\SingleVideos\\")
 
-#yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
-#yt.download(".\\SingleVideos\\")
+	# Creating YouTube object with the link
 
-#PLAYLIST DOWNLOAD
-# Create Playlist Directory if it doesn't exist
-if not os.path.exists("Playlists"):
-    os.mkdir("Playlists")
-    print("Directory " , "Playlists" ,  " Created ")
-else:    
-    print("Directory " , "Playlists" ,  " already exists")
+	myVideo = YouTube(downloadLink)
+
+	# Title of the Video
+
+	print("Title: " + myVideo.title)
+
+	# Length of the Video in Seconds
+
+	print("Duration: " + myVideo.length)
+
+	# Description of the Video
+
+	#print("Description: " + myVideo.description)
+
+	# Total Views of the Video
+
+	print("Views: " + myVideo.views)
+
+	# Age Restricted Content
+
+	print("Age Restricted: " + str(myVideo.age_restricted))
+
+	# ID of the Video
+
+	print("Video ID: " + myVideo.video_id)
+
+	myStream = YouTube(downloadLink).streams.first()
+
+	myStream.download(".\\SingleVideos\\")
+		#SINGLE IMAGE DOWNLOAD (Backup)
+		#yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
+		#yt.download(".\\SingleVideos\\")
+
+	
+def playlistDownload():
+	# Create Playlist Directory if it doesn't exist
+	if not os.path.exists("Playlists"):
+	    os.mkdir("Playlists")
+	    print("Directory " , "Playlists" ,  " Created ")
+	else:    
+	    print("Directory " , "Playlists" ,  " already exists")
+
+	
+	#Playlist download
+	playlistName = "Unknown"
+	pl = Playlist(downloadLink)
+	playlistName = pl.title()
+
+	# Create target Directory for downloaded playlist
+	try:
+	    os.mkdir("Playlists\\" + playlistName)
+	    print("Directory " , "Playlists\\" + playlistName ,  " Created ") 
+	except FileExistsError:
+	    print("Directory " , "Playlists\\" + playlistName ,  " already exists")
+
+	#pl.download_all()
+	pl.download_all(".\\Playlists\\" + playlistName + "\\")
 
 
-pl = Playlist("https://www.youtube.com/watch?v=Edpy1szoG80&list=PL153hDY-y1E00uQtCVCVC8xJ25TYX8yPU")
-
-playlistName = pl.title()
-
-try:
-    # Create target Directory
-    os.mkdir("Playlists\\" + playlistName)
-    print("Directory " , "Playlists\\" + playlistName ,  " Created ") 
-except FileExistsError:
-    print("Directory " , "Playlists\\" + playlistName ,  " already exists")
-
-#pl.download_all()
-#pl.download_all(".\\Playlists\\" + playlistName + "\\")
-
-#video = YouTube('https://www.youtube.com/watch?v=d3D7Y_ycSms')
-#print(video.thumbnail_url)
 
 
-#yt = YouTube(downloadLink)
-#yt.thumbnail_url
+if downloadLink.find("list") == -1:
+	singleVideoDownload()
+else:
+	playlistDownload()
